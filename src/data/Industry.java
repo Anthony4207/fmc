65f123bcf69e1d8551b69a29e40b9279a5103e34
3103503213 <3103503213@CCI-12975.tafe.mst>
2015-06-18 08:57:59 +1000

    Created Category object Created Industry object

---
+++ src/data/Industry.java
@@ -0,0 +1,50 @@
+package data;
+
+/**
+ *
+ * @author 3103503213
+ */
+public class Industry
+{
+
+    private int industryID;
+    private String industryName;
+
+    public Industry(int industryID, String industryName)
+    {
+	this.industryID = industryID;
+	this.industryName = industryName;
+    }
+
+    /**
+     * @return the industryID
+     */
+    public int getIndustryID()
+    {
+	return industryID;
+    }
+
+    /**
+     * @param industryID the industryID to set
+     */
+    public void setIndustryID(int industryID)
+    {
+	this.industryID = industryID;
+    }
+
+    /**
+     * @return the industryName
+     */
+    public String getIndustryName()
+    {
+	return industryName;
+    }
+
+    /**
+     * @param industryName the industryName to set
+     */
+    public void setIndustryName(String industryName)
+    {
+	this.industryName = industryName;
+    }
+}
