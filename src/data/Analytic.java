66c1e173b9766a2ad65353bcb3f83ab7123b7215
6100265615 <6100265615@CCI-12972.tafe.mst>
2015-06-18 11:05:08 +1000

    Created Analytic object

---
+++ src/data/Analytic.java
@@ -0,0 +1,85 @@
+package data;
+
+import java.sql.Date;
+
+
+/**
+ *
+ * @author 6100265615
+ */
+public class Analytic {
+    
+    private int analyticID;
+    private int userID;
+    private int courseID;
+    private Date dateAccessed;
+         
+    
+    public Analytic(int analyticID, int userID, int courseID, Date dateAccessed) {
+
+this.analyticID = analyticID;
+this.userID = userID;
+this.courseID = courseID;
+this.dateAccessed = dateAccessed;
+
+}
+
+    /**
+     * @return the analyticID
+     */
+    public int getAnalyticID() {
+        return analyticID;
+    }
+
+    /**
+     * @param analyticID the analyticID to set
+     */
+    public void setAnalyticID(int analyticID) {
+        this.analyticID = analyticID;
+    }
+
+    /**
+     * @return the userID
+     */
+    public int getUserID() {
+        return userID;
+    }
+
+    /**
+     * @param userID the userID to set
+     */
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    /**
+     * @return the courseID
+     */
+    public int getCourseID() {
+        return courseID;
+    }
+
+    /**
+     * @param courseID the courseID to set
+     */
+    public void setCourseID(int courseID) {
+        this.courseID = courseID;
+    }
+
+    /**
+     * @return the dateAccessed
+     */
+    public Date getDateAccessed() {
+        return dateAccessed;
+    }
+
+    /**
+     * @param dateAccessed the dateAccessed to set
+     */
+    public void setDateAccessed(Date dateAccessed) {
+        this.dateAccessed = dateAccessed;
+    }
+    
+}
+
+
